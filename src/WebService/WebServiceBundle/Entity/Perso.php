051894ff7cbc95496cfbc874405bf36429a766af
romain <romain.toma@bbox.fr>
2015-02-26 19:02:16 +0100

    Add self-referencing in perso + add phpDocumentor bundle

--- src/WebService/WebServiceBundle/Entity/Perso.php
+++ src/WebService/WebServiceBundle/Entity/Perso.php
@@ -8,6 +8,7 @@ use JMS\Serializer\Annotation\Expose;
 use JMS\Serializer\Annotation\Groups;
 use JMS\Serializer\Annotation\MaxDepth;
 use JMS\Serializer\Annotation\ExclusionPolicy;
+use Symfony\Component\Config\Definition\IntegerNode;
 
 /**
  * @ORM\Entity
@@ -60,10 +61,34 @@ class Perso extends BaseEntity
 	 */
 	private $register;
 	
+	/**
+	 * 
+	 * @var integer
+	 * @ORM\ManyToOne(targetEntity="Perso", inversedBy="contact")
+	 * @ORM\JoinColumn(name="parent_id", referencedColumnName="id")
+	 */
+	private $parent;
+	
+	/**
+	 * 
+	 * @var Integer
+	 * @ORM\OneToMany(targetEntity="Perso", mappedBy="parent")
+	 * @Expose()
+	 * @Groups({"ById"})
+	 * @MaxDepth(2)
+	 */
+	private $contact;
+	
+	
+	public function __construct(){
+		$this->setContact(new \Doctrine\Common\Collections\ArrayCollection());
+	}
+	
 	public function getName()
-	 {
+	{
 		return $this->name;
 	}
+	
 	public function setName($name) 
 	{
 		$this->name = $name;
@@ -74,6 +99,7 @@ class Perso extends BaseEntity
 	{
 		return $this->level;
 	}
+	
 	public function setLevel($level) 
 	{
 		$this->level = $level;
@@ -84,6 +110,7 @@ class Perso extends BaseEntity
 	{
 		return $this->class;
 	}
+	
 	public function setClass($class) 
 	{
 		$this->class = $class;
@@ -94,6 +121,7 @@ class Perso extends BaseEntity
 	{
 		return $this->race;
 	}
+	
 	public function setRace($race) 
 	{
 		$this->race = $race;
@@ -104,26 +132,47 @@ class Perso extends BaseEntity
 	{
 		return $this->sexe;
 	}
+	
 	public function setSexe($sexe) 
 	{
 		$this->sexe = $sexe;
 		return $this;
 	}
+	
 	public function getStuff() {
 		return $this->stuff;
 	}
+	
 	public function setStuff($stuff) {
 		$this->stuff = $stuff;
 		return $this;
 	}
+	
 	public function getRegister() {
 		return $this->register;
 	}
+	
 	public function setRegister($register) {
 		$this->register = $register;
 		return $this;
 	}
+	public function getParent() {
+		return $this->parent;
+	}
 	
+	public function setParent($parent) {
+		$this->parent = $parent;
+		return $this;
+	}
+	
+	public function getContact() {
+		return $this->contact;
+	}
+	
+	public function setContact($contact) {
+		$this->contact = $contact;
+		return $this;
+	}	
 	
 	public function __toString(){
 		return $this->name;
